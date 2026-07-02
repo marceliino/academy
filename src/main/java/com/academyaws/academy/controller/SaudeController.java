ecf937fba3ddea47fb95fb08a9c9d1baba1d49d5
Marcelo SiLva <marceliino1@gmail.com>
2026-07-02 20:22:37 -0300

    melhorias

---
+++ src/main/java/com/academyaws/academy/controller/SaudeController.java
@@ -0,0 +1,30 @@
+package com.academyaws.academy.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class SaudeController {
+
+    @GetMapping("saudeApp")
+    public ResponseEntity<Map<String, String>> verificaSaudeApp() {
+
+        Date dataHoraAtual = new Date();
+        SimpleDateFormat formatador = new SimpleDateFormat("HH:mm:ss");
+        String horaFormatada = formatador.format(dataHoraAtual);
+
+        System.out.println("Hora atual: " + horaFormatada);
+
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "Running");
+        response.put("hora", horaFormatada);
+
+        return ResponseEntity.ok(response);
+    }
+}
