49870e9143a89d2d7c1380e8ecf131283d7d6ead
Marcelo SiLva <marceliino1@gmail.com>
2026-07-06 22:49:54 -0300

    delete classe cep

--- src/main/java/com/academyaws/academy/cep/repository/CepRepository.java
+++
@@ -1,8 +0,0 @@
-package com.academyaws.academy.cep.repository;
-
-import com.academyaws.academy.cep.entity.Cep;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CepRepository  extends JpaRepository<Cep, Long> {
-}
-
