0e832e8b7d817144bf97511529bd62fee1686e99
Marcelo SiLva <marceliino1@gmail.com>
2026-07-09 21:02:54 -0300

    Implementacao sqs

---
+++ src/main/java/com/academyaws/academy/sqs/controller/SqsController.java
@@ -0,0 +1,34 @@
+package com.academyaws.academy.sqs.controller;
+
+import com.academyaws.academy.sqs.service.SqsService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/sqs")
+public class SqsController {
+
+    private final SqsService service;
+
+    public SqsController(SqsService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/send")
+    public ResponseEntity<String> send(@RequestParam String message) {
+        return ResponseEntity.ok(service.sendMessage(message));
+    }
+
+    @GetMapping("/process")
+    public ResponseEntity<List<Map<String, String>>> process() {
+        return ResponseEntity.ok(service.receiveAndProcess());
+    }
+
+    @GetMapping("/peek")
+    public ResponseEntity<List<String>> peek() {
+        return ResponseEntity.ok(service.peekMessages());
+    }
+}
